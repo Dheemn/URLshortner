2438305bc1be0dfb0609eddfedeb3f037558102b
Dheemn <dheemu2000@gmail.com>
2022-07-14 19:20:01 +0530

    Linting changes

--- lib/blacklist.py
+++ lib/blacklist.py
@@ -2,18 +2,20 @@
 
 from lib.responses import CommonResponses
 
-#This gives the list of blacklisted paths(temporary just to get the app working)
+
+# This gives the list of blacklisted paths(temporary just to get the
+# app working)
 class Blacklist():
-    
+
     def __init__(self):
         self._blacklist = [
                 "/favicon.ico",
                 ]
         self._cR = CommonResponses()
-    
+
     def hole(self, path):
         path = "/"+path
         for i in self._blacklist:
-            if ( i == path ):
+            if (i == path):
                 return self._cR.type('403')
         return None
